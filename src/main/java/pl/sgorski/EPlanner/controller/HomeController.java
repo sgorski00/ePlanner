0d4e53aed5ca30238885847aea6ea120be8c55c0
Sebastian Górski <sebastian.gorski@studenci.collegiumwitelona.pl>
2025-03-11 19:15:48 +0100

    basic page template with bootstrap

---
+++ src/main/java/pl/sgorski/EPlanner/controller/HomeController.java
@@ -0,0 +1,15 @@
+package pl.sgorski.EPlanner.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "home";
+    }
+}
