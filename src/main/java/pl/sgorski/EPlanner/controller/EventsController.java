c1e1fa1190f1ac5902e674f7db95a5fe2098071d
Sebastian Górski <sebastiangorski00@gmail.com>
2025-03-12 14:30:40 +0100

    add save new event

--- src/main/java/pl/sgorski/EPlanner/controller/EventsController.java
+++ src/main/java/pl/sgorski/EPlanner/controller/EventsController.java
@@ -1,12 +1,11 @@
 package pl.sgorski.EPlanner.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.sgorski.EPlanner.model.Event;
 import pl.sgorski.EPlanner.service.EventService;
@@ -67,5 +66,30 @@ public class EventsController {
             return "redirect:/events";
         }
         return "event";
+    }
+
+    @GetMapping("/add")
+    public String showAddEventForm(
+            Model model
+    ){
+        model.addAttribute("event", new Event());
+        model.addAttribute("today", LocalDate.now());
+        return "add_event";
+    }
+
+    @PostMapping("/add")
+    public String addEvent(
+            @Valid @ModelAttribute("event") Event event,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes
+    ){
+        if(bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute("error", "Something went wrong.");
+            return "redirect:/events/add";
+        }
+
+        eventService.save(event);
+        redirectAttributes.addFlashAttribute("info", "Event added succesfully");
+        return "redirect:/events/add";
     }
 }
--- src/main/java/pl/sgorski/EPlanner/model/Event.java
+++ src/main/java/pl/sgorski/EPlanner/model/Event.java
@@ -1,12 +1,14 @@
 package pl.sgorski.EPlanner.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -29,9 +31,27 @@ public class Event {
     private LocalTime hour;
 
     @Column(nullable = false)
+    @Size(max = 100)
     private String name;
 
+    @Size(max = 255)
     private String description;
 
     private Timestamp createdAt;
+    private Timestamp lastEdit;
+    private Timestamp finishedAt;
+    private Timestamp archivedAt;
+
+    @PrePersist
+    public void setCreatedAt(){
+        if(createdAt == null) {
+            this.createdAt = Timestamp.from(Instant.now());
+            this.lastEdit = null;
+        }
+    }
+
+    @PreUpdate
+    public void setLastEdit(){
+        this.lastEdit = Timestamp.from(Instant.now());
+    }
 }
