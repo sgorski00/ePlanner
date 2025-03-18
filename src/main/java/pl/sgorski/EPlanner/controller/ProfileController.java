eebacb93ac636000f7f5cd5ca3ae591b5f303838
Sebastian Górski <sebastian.gorski@studenci.collegiumwitelona.pl>
2025-03-18 18:10:37 +0100

    add change password form

--- src/main/java/pl/sgorski/EPlanner/controller/ProfileController.java
+++ src/main/java/pl/sgorski/EPlanner/controller/ProfileController.java
@@ -1,10 +1,13 @@
 package pl.sgorski.EPlanner.controller;
 
+import jakarta.validation.Valid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.sgorski.EPlanner.model.ApplicationUser;
 import pl.sgorski.EPlanner.service.UserService;
@@ -16,6 +19,7 @@ import java.util.NoSuchElementException;
 @Controller
 public class ProfileController {
 
+    private static final Logger log = LogManager.getLogger(ProfileController.class);
     private final UserService userService;
 
     @Autowired
@@ -38,5 +42,23 @@ public class ProfileController {
             return "redirect:/";
         }
         return "profile";
+    }
+
+    @PostMapping("/change_password")
+    public String changePassword(
+            @RequestParam("password") String password,
+            RedirectAttributes redirectAttributes,
+            Principal principal
+    ) {
+        try {
+            ApplicationUser user = userService.findByUsername(principal.getName());
+            user.setPassword(password);
+            userService.save(user);
+        }  catch (NoSuchElementException e) {
+            redirectAttributes.addFlashAttribute("error", "User not found");
+            return "redirect:/";
+        }
+        redirectAttributes.addFlashAttribute("info", "Password changed successfully");
+        return "redirect:/profile";
     }
 }
